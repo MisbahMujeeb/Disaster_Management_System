958ec44dce7df56e80a7af55b83108c5c3f5510f
MisbahMujeeb <misbah1810c@aptechgdn.net>
2020-11-04 14:40:49 +0500

    Splash Screen

--- app/src/main/java/com/example/disastermanegementsystem/MainActivity.java
+++ app/src/main/java/com/example/disastermanegementsystem/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.disastermanegementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signUp,signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signIn =findViewById(R.id.btnSignIn);
+        signUp=findViewById(R.id.btnSignUp);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(i);
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
